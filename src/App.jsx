abca8ee140761931e23e2a3734f4286addd4be0a
Sma-source <rabhi.smain@gmail.com>
2023-06-13 22:03:54 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { Scroll, ScrollControls } from "@react-three/drei";
 import Interface from "./components/Interface";
 import ScrollManager from "./components/ScrollManager";
 import Menu from "./components/Menu";
+import { MotionConfig } from "framer-motion";
+import { Leva } from "leva";
+import { Cursor } from "./components/Cursor";
 
 function App() {
   const [section, setSection] = useState(0);
@@ -15,21 +18,35 @@ function App() {
   }, [section]);
   return (
     <>
-      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-        <color attach="background" args={["#ececec"]} />
-        <ScrollControls pages={4} damping={0.1}>
-          <ScrollManager section={section} onSectionChange={setSection} />
-          <Experience />
-          <Scroll html>
-            <Interface />
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
-      <Menu
-        onSectionChange={setSection}
-        menuOpened={menuOpened}
-        setMenuOpened={setMenuOpened}
-      />
+      <MotionConfig
+        transition={{
+          type: "spring",
+          mass: 5,
+          stiffness: 50,
+          damping: 50,
+          restDelta: 0.0001,
+        }}
+      >
+        <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+          <color attach="background" args={["#ececec"]} />
+          <ScrollControls pages={4} damping={0.1}>
+            <ScrollManager section={section} onSectionChange={setSection} />
+            <Scroll>
+              <Experience section={section} menuOpened={menuOpened} />
+            </Scroll>
+            <Scroll html>
+              <Interface />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+        <Menu
+          onSectionChange={setSection}
+          menuOpened={menuOpened}
+          setMenuOpened={setMenuOpened}
+        />
+        <Cursor />
+      </MotionConfig>
+      <Leva hidden />
     </>
   );
 }
